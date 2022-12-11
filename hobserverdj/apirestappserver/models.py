c397c5468b0210840c8cad281f4f471db5152c20
Federico Toledo <federicotoledobaeza@gmail.com>
2022-12-11 21:53:26 +0100

    save version

--- hobserverdj/apirestappserver/models.py
+++ hobserverdj/apirestappserver/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.db import models
 from django.contrib.auth.models import PermissionsMixin, User
 from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
-from django.utils.timezone import now
+from django.utils import timezone
 
 
 # Create your models here.
@@ -16,7 +16,7 @@ class UserManager(BaseUserManager):
             raise ValueError("the given email is not valid")
         email = self.normalize_email(email)
         user = self.model(username=username, email=email, is_active=True, is_staff=is_staff, is_superuser=is_superuser,
-                          date_joined=now(), **extrafields)
+                          date_joined=timezone.now(), **extrafields)
         user.set_password(password)
         user.save(using=self.db)
 
@@ -55,8 +55,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-    date_joined = models.DateTimeField(default=now())
-    birth_date = models.DateField(default=now())
+    date_joined = models.DateTimeField(default=timezone.now())
+    birth_date = models.DateField(default=timezone.now())
     adress = models.CharField(max_length=300,default="default adress")
     city = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
@@ -79,7 +79,7 @@ class UsersGroups(models.Model):
     description = models.TextField(max_length=300)
     image_url = models.URLField(default="https://cdn0.iconfinder.com/data/icons/"
                                 "users-android-l-lollipop-icon-pack/24/group2-512.png")
-    created_at = models.DateTimeField(default=now())
+    created_at = models.DateTimeField(default=timezone.now())
     owner = models.ForeignKey(User, related_name="g_user", on_delete=models.SET_NULL, blank=True, null=True)
     members = models.ManyToManyField(User, related_name="g_members", through='Membership')
 
@@ -105,7 +105,7 @@ class Membership(models.Model):
     objects = models.Manager
     user = models.ForeignKey(User, related_name="memb_user", on_delete=models.SET_NULL, blank=True, null=True)
     group = models.ForeignKey(UsersGroups, related_name="memb_group", on_delete=models.SET_NULL, blank=True, null=True)
-    date_joined = models.DateTimeField(default=now())
+    date_joined = models.DateTimeField(default=timezone.now())
     invite_reason = models.CharField(max_length=128)
 
 
@@ -118,8 +118,8 @@ class Rooms(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=300)
     image_url = models.URLField()
-    created_at = models.DateTimeField(default=now())
-    updated_at = models.DateTimeField(default=now())
+    created_at = models.DateTimeField(default=timezone.now())
+    updated_at = models.DateTimeField(default=timezone.now())
     group = models.ForeignKey(UsersGroups, related_name="r_group", on_delete=models.CASCADE)
     owner = models.ForeignKey(User, related_name="r_user", on_delete=models.SET_NULL, blank=True, null=True)
 
@@ -129,8 +129,8 @@ class Rooms(models.Model):
 
 class Messages(models.Model):
     content = models.TextField()
-    created_at = models.DateTimeField(default='django.utils.timezone.now')
-    updated_at = models.DateTimeField(default='django.utils.timezone.now')
+    created_at = models.DateTimeField(default=timezone.now())
+    updated_at = models.DateTimeField(default=timezone.now())
     room = models.ForeignKey(Rooms, related_name="m_room", on_delete=models.CASCADE)
     owner = models.ForeignKey(User, related_name="m_user", on_delete=models.SET_NULL, blank=True, null=True)
 
